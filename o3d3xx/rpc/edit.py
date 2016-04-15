d4e78302ad2c2309633cce505a3b5f790f5c5399
Christoph Freundl <christoph.freundl@ifm.com>
2016-04-15 10:19:03 +0200

    Application proxies are setup in Edit object
    Signed-off-by: Christoph Freundl <christoph.freundl@ifm.com>

--- o3d3xx/rpc/edit.py
+++ o3d3xx/rpc/edit.py
@@ -8,8 +8,19 @@ class Edit:
 		self.rpc = xmlrpc.client.ServerProxy(self.url)
 		self.deviceURL = self.url + 'device/'
 		self.device = xmlrpc.client.ServerProxy(self.deviceURL)
-		self.networkURL = self.url + 'network/'
+		self.networkURL = self.deviceURL + 'network/'
 		self.network = xmlrpc.client.ServerProxy(self.networkURL)
+		self.updateAppUrl()
+
+	def updateAppUrl(self):
+		self.applicationURL = self.url + 'application/'
+		self.application = xmlrpc.client.ServerProxy(self.applicationURL)
+		self.imagerConfigURL = self.applicationURL + 'imager_001/'
+		self.imagerConfig = xmlrpc.client.ServerProxy(self.imagerConfigURL)
+		self.spatialFilterURL = self.imagerConfigURL + 'spatialfilter/'
+		self.spatialFilter = xmlrpc.client.ServerProxy(self.spatialFilterURL)
+		self.temporalFilterURL = self.imagerConfigURL + 'temporalfilter/'
+		self.temporalFilter = xmlrpc.client.ServerProxy(self.temporalFilterURL)
 
 	def __getattr__(self, name):
 		# Forward otherwise undefined method calls to XMLRPC proxy
--- o3d3xx/rpc/session.py
+++ o3d3xx/rpc/session.py
@@ -20,22 +20,11 @@ class Session:
 	def startEdit(self):
 		self.rpc.setOperatingMode(1)
 		self.edit = Edit(self.url + 'edit/')
-		self.updateAppUrl()
 		return self.edit
 
 	def stopEdit(self):
 		self.rpc.setOperatingMode(0)
 		self.edit = None
-
-	def updateAppUrl(self):
-		self.applicationURL = self.url + 'application/'
-		self.application = xmlrpc.client.ServerProxy(self.applicationURL)
-		self.imagerConfigURL = self.applicationURL + 'imager_001/'
-		self.imagerConfig = xmlrpc.client.ServerProxy(self.imagerConfigURL)
-		self.spatialFilterURL = self.imagerConfigURL + 'spatialfilter/'
-		self.spatialFilter = xmlrpc.client.ServerProxy(self.spatialFilterURL)
-		self.temporalFilterURL = self.imagerConfigURL + 'temporalfilter/'
-		self.temporalFilter = xmlrpc.client.ServerProxy(self.temporalFilterURL)
 
 	def __getattr__(self, name):
 		# Forward otherwise undefined method calls to XMLRPC proxy
