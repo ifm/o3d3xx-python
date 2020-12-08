276cf68601bb54fd54a741ea7765e3993bae03d6
Christian Ege <christian.ege@ifm.com>
2020-12-08 20:41:00 +0100

    Use the right axis
    Signed-off-by: Christian Ege <christian.ege@ifm.com>

--- examples/image_viewer.py
+++ examples/image_viewer.py
@@ -31,10 +31,14 @@ def updatefig(*args):
     imAmp = args[2]
     amp_max = float(max(np.max(g.Amplitude),1));
     imAmp.set_array(g.Amplitude/ amp_max)
+    axAmp = imAmp.axes
+    axAmp.set_title('Amplitude (Illu temp: %0.2f)'%(g.illuTemp))
 
     imDist = args[3]
     dist_max = float(max(np.max(g.Distance),1));
     imDist.set_array(g.Distance/ dist_max)
+    axDist = imDist.axes
+    axDist.set_title('Distance')
     return imAmp,imDist,
 
 def main():
@@ -44,9 +48,7 @@ def main():
     fig = plt.figure()
     grabber = GrabO3D300(camData)
     ax1 = fig.add_subplot(1, 2, 1)
-    ax1.set_title('Amplitude')
     ax2 = fig.add_subplot(1, 2, 2)
-    ax2.set_title('Distance')
     imAmplitude = ax1.imshow(np.random.rand(imageHeight,imageWidth))
     imDistance = ax2.imshow(np.random.rand(imageHeight,imageWidth))
     ani = animation.FuncAnimation(fig, updatefig, interval=50, blit=True, fargs = [grabber,imAmplitude,imDistance])
